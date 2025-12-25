a932406a48d8c4f2594d77ee3a679e1430ee979d
joelrealubit <joel.realubit@gmail.com>
2025-12-26 03:50:05 +0800

    added player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,15 +12,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+        
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60)/1000
-        print(dt)
+        #print(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        
 
 if __name__ == "__main__":
     main()
