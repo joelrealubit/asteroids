3ab77c3e2d0cee67bb5497258a8c184f19a237c6
joelrealubit <joel.realubit@gmail.com>
2025-12-26 02:55:08 +0800

    fixed return indent after if

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
@@ -15,7 +14,7 @@ def main():
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-            return
+                return
         screen.fill("black")
         pygame.display.flip()
 if __name__ == "__main__":
